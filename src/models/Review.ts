bf4e341c392fbe981755d0d420e79c6b66a9589a
HyeokJoon Kong <orijoon98@gmail.com>
2022-05-21 15:24:39 +0900

    [FEAT] timestamp 추가

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -1,22 +1,27 @@
 import mongoose from "mongoose";
 import { ReviewInfo } from "../interfaces/review/ReviewInfo";
 
-const ReviewScheme = new mongoose.Schema({
-    user: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: "User",
+const ReviewScheme = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        post: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+            ref: "Post",
+        },
+        text: {
+            type: String,
+            required: true,
+        },
     },
-    post: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: "Post",
-    },
-    text: {
-        type: String,
-        required: true,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 export default mongoose.model<ReviewInfo & mongoose.Document>(
     "Review",
